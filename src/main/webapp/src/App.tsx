e8a425d362bb5bff85058be7a0f55a5927ea9dd2
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-09-15 03:19:10 -0300

    chore: add lint and prettier to the react project

---
+++ src/main/webapp/src/App.tsx
@@ -0,0 +1,9 @@
+import Button from './components/button/Button';
+
+const App = () => (
+  <div className='app' style={{ padding: '20px' }}>
+    <Button />
+  </div>
+);
+
+export default App;
---
+++ src/main/webapp/src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className='button' onClick={() => {}}>
+      Hello World!
+    </button>
+  );
+};
+
+export default Button;
