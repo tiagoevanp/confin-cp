a0650b619da10628024e36f889b9a539740479a5
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2023-11-12 16:24:47 -0300

    fix: NaN in money mask

--- src/main/webapp/src/hooks/useMoneyMask.ts
+++ src/main/webapp/src/hooks/useMoneyMask.ts
@@ -3,12 +3,12 @@ import { useCallback } from 'react';
 type useMoneyMaskReturn = (value: string) => string;
 
 export const moneyMask = (value: string): string => {
-  if (value === '') return value;
-
   const onlyNumbers = value
     .replace(/[^0-9]/, '')
     .replace(',', '')
     .replace('.', '');
+
+  if (onlyNumbers === '') return '';
 
   return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(
     parseFloat(onlyNumbers) / 100,
